49ade41302995b8e04706a9aa3d03527d9d3027b
mariaeugenia907 <77995348+mariaeugenia907@users.noreply.github.com>
2021-01-27 15:20:11 -0300

    Add files via upload

---
+++ atividade4/q1.java
@@ -0,0 +1,10 @@
+package q1;
+
+import java.util.Scanner; package br.ifpi.poo.ex05.cadastro;
+public class ExecutaBanco {
+....
+}
+
+Ocorreu um erro durante a inicialização, 
+não acha o pacote pedido, 
+e não chamarmos esse pacote ele não consegue enxergar que tal classe pertence ao pacote x.
